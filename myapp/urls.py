11b1f944c0b1e2c737bbca3bb5b9c8d45b14af73
munnurusriram100 <munnurusriram100@gmail.com>
2026-09-24 15:30:07 +0530

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from myapp import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('register/',views.register,name='signup'),
+    path('edit/<int:id>',views.edit,name='modify'),
+    path('find/<int:id>',views.find,name='search'),
+    path('del/<int:id>',views.delemployee),
+    path('employees/',views.getemployees,name='emps'),
+]
